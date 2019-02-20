224bf6a8fa96718a9eeadb6b284e502969f74281
Ryad <selatniryadg@gmail.com>
2019-02-20 11:08:09 +0100

    new db schema

--- Serveur/DataBase/InsertExemple.sql
+++ Serveur/DataBase/InsertExemple.sql
@@ -1,9 +1,10 @@
+USE CodingFactoryPreAdmission;
 
 /* Création d'un user */ #niveau 3 : GodMode #niveau 2 : Admin #niveau 1 : user
-INSERT INTO Users (lastName, firstName, email, userPassword,lawLevel, testDate)
+INSERT INTO Users (lastName, firstName, email, userPassword,lawLevel, testDate, birthdate)
  VALUES
- ('Selatni', 'Ryad', 'selatniryadg@gmail.com', 'myPassWord', 3, null),
- ('Drieux', 'Alan', 'alan.drieux@outlook.fr','password', 3, null);
+ ('Selatni', 'Ryad', 'selatniryadg@gmail.com', 'myPassWord', 3, null, NOW()),
+ ('Drieux', 'Alan', 'alan.drieux@outlook.fr','password', 3, null, NOW());
 select * from Users;
 
 /* Création d'un type de question */
@@ -21,19 +22,19 @@ VALUES
 select * from ProgLanguage;
 
 /* Insertion des questions */
-INSERT INTO Question(questionText, categoryId, progLanguageId)
+INSERT INTO Question(questionText, categoryId, progLanguageId, difficulty)
 VALUES 
-('Qui est Joey Star ?', 1, null),
-('En java, comment déclarer une variable globale ?', null, 2);
+('Qui est Joey Star ?', 1, null, 1),
+('En java, comment déclarer une variable globale ?', null, 2, 1);
 select * from Question;
 
 /* Insertion des questions */
-INSERT INTO Choice(textResponse, questionId, rightAnswer)
+INSERT INTO Choice(textResponse, questionId, rightAnswer, IDontKnow)
 VALUES 
-('Un acteur ', 1, false),
-('Un Rappeur', 1, false),
-('Un personage fictif', 1, false),
-('Un maltraiteur de petit singe', 1, true);
+('Un acteur ', 1, false,false),
+('Un Rappeur', 1, false,false),
+('Un personage fictif', 1, false,false),
+('Un maltraiteur de petit singe', 1, true,false);
 select * from Choice;
 
 /* Insertion de la session des résultats de l'étudiant APRES sa session */
@@ -41,9 +42,10 @@ INSERT INTO SessionStudent (userId, beginningSession, endSession, questionsAnswe
 VALUES
 (1, NOW(), NOW(), 21, 2094);
 
-INSERT INTO Answers (userId, questionId, rightAnswer, choiceId)
+
+/*INSERT INTO Answers (userId, questionId, rightAnswer, choiceId)
 VALUES
-(1, 2, true, 4);
+(1, 2, true, '1,3');*/
 
 
 
