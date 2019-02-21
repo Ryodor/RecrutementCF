2f705537d88dfcd5ee9bc40520b214c6f08432d0
Alan Drieux <alan.drieux@outlook.fr>
2019-02-21 16:35:30 +0100

    add Route /api/users/lang for send all langage programation and add /api/users/formations for send all fromations informations

--- Server/api/qcm.js
+++ Server/api/qcm.js
@@ -19,20 +19,38 @@ router.get('/', function (req, res, next) {
     })
 
 })
-db.execute
+//b.execute
 router.get('/start', function (req, res, next) {
 
-    db.execute('SELECT * FROM `Categories` WHERE `ID` = ?',[1],function(error, results, fields){
-
-    })
     /* Creation  d'un template de QCM est l'envoie en json */
-    db.execute('SELECT * FROM `Choice`,`Question` WHERE `Choice`.`questionId` = `Question`.`ID` AND `Question`.`categoryId` = ?', [1],function(error, results, fields){
+    let Categories;
+    let Questions;
+    let Choice;
+
+    db.execute('SELECT * FROM `Categories` ',function(error, results, fields){
+        if (error) throw error;
+        else if(results.length > 0){
+            Categories = results;
+        }
+    })
+
+    db.execute('SELECT * FROM `Question` WHERE  `categoryId` = ?', [1],function(error, results, fields) {
+        if (error) throw error;
+
+        if (results.length > 0) {
+
+        }else
+            return res.send({response:"",error:"Aucune question n\'a était trouver."})
+    })
+
+    db.execute('SELECT * FROM `Choice` WHERE `questionId` = ?', [1],function(error, results, fields){
         if (error) throw error;
 
         if (results.length > 0) {
             return res.send({
                 response: {
                     sessionID: req.sessionID,
+                    categories : Categories,
                     question: results
                 }, error: ""
             });
--- Server/database.js
+++ Server/database.js
@@ -1,15 +1,14 @@
 const database = require('mysql2');
 require('dotenv').config();
 
-let db;
-if(db == null || db == undefined){
-    console.log("[database] database initialiser.");
-    db = database.createConnection({
+async function connectToDatabase(){
+    console.log("[database] initialize db.")
+    return await database.createConnection({
         host     : process.env.DB_HOST,
         user     : process.env.DB_USER,
         password : process.env.DB_PASS,
         database : process.env.DB_NAME,
-    });
+    })
 }
 
-module.exports = db;
+module.exports = connectToDatabase()
