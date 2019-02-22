d35ac4d2cff9e4a8007f51bfcae53f29afec41f9
Alan Drieux <alan.drieux@outlook.fr>
2019-02-22 15:30:09 +0100

    change register for add new informations, WIP QCM route

--- Server/api/qcm.js
+++ Server/api/qcm.js
@@ -2,63 +2,92 @@ let express = require('express');
 let router = express.Router();
 
 router.get('/', function (req, res, next) {
-
-    /* Creation  d'un template de QCM est l'envoie en json */
-    db.execute('SELECT * FROM `Choice`,`Question` WHERE `Choice`.`questionId` = `Question`.`ID` AND `Question`.`ID` = 1',function(error, results, fields){
-        if (error) throw error;
-
-        if (results.length > 0) {
-            return res.send({
-                response: {
-                    sessionID: req.sessionID,
-                    question: results
-                }, error: ""
-            });
-        }else
-            return res.send({response:"",error:"Aucune question n\'a était trouver."})
-    })
-
+    res.send("Il n'y a rien ici :)")
 })
-//b.execute
+
 router.get('/start', function (req, res, next) {
+     //if(req.user){
+        /* Creation  d'un template de QCM est l'envoie en json */
+        let Categories;
+        let Langages;
+        let question;
 
-    /* Creation  d'un template de QCM est l'envoie en json */
-    let Categories;
-    let Questions;
-    let Choice;
+        db.execute('SELECT * FROM `Categories` ',function(error, results, fields){
+            if (error) throw error;
+            else if(results.length > 0){
+                Categories = results;
+            }
+        })
 
-    db.execute('SELECT * FROM `Categories` ',function(error, results, fields){
-        if (error) throw error;
-        else if(results.length > 0){
-            Categories = results;
-        }
-    })
+        db.execute('SELECT * FROM `ProgLanguage` ',function(error, results, fields){
+            if (error) throw error;
+            else if(results.length > 0){
+                Langages = results;
+            }
+        })
 
-    db.execute('SELECT * FROM `Question` WHERE  `categoryId` = ?', [1],function(error, results, fields) {
-        if (error) throw error;
 
-        if (results.length > 0) {
+        generateQuestionsByCategory(1,[1,2])
+            .then(question=>{
+                console.log("[DEBUG] question1 ",question)
 
-        }else
-            return res.send({response:"",error:"Aucune question n\'a était trouver."})
-    })
+                db.execute('SELECT * FROM `Choice` WHERE `questionId` = ?', [question[0].ID],function(error, results, fields){
+                     if (error) throw error;
 
-    db.execute('SELECT * FROM `Choice` WHERE `questionId` = ?', [1],function(error, results, fields){
-        if (error) throw error;
-
-        if (results.length > 0) {
-            return res.send({
-                response: {
-                    sessionID: req.sessionID,
-                    categories : Categories,
-                    question: results
-                }, error: ""
-            });
-        }else
-            return res.send({response:"",error:"Aucune question n\'a était trouver."})
-    })
+                     if (results.length > 0) {
+                         return res.send({
+                             response: {
+                                 sessionID: req.sessionID,
+                                 categories : Categories,
+                                 langages : Langages,
+                                 question: question[0],
+                                 choice: results
+                             }, error: ""
+                         });
+                     }else
+                         return res.send({response:"",error:"Aucune choix n\'a était trouver."})
+                })
+            })
+            .catch(error=>{
+                console.log(error)
+            })
+    //}else
+    //    return res.send({response:"",error:"Vous n'êtes la connecter a un compte."})
 
 })
 
+router.get('/question/:id', function (req, res, next) {
+
+})
+
+// =================================================================
+
+function generateQuestionsByCategory(catgeroyId,langIds){
+    let queryLangId = "";
+    if(langIds.length > 1){
+        queryLangId += "("
+        langIds.forEach((lang, index)=>{
+            if(index == langIds.length-1)
+                queryLangId += "langId = "+lang
+            else
+                queryLangId += "langId = "+lang+" OR "
+        })
+        queryLangId += ")"
+    }else
+        queryLangId = langIds[0]
+
+    console.log("[debug] queryLangId ",queryLangId)
+    return new Promise((resolve, reject) => {
+        db.execute('SELECT * FROM `Question` WHERE  `categoryId` = ? '+queryLangId+' ORDER BY RAND(), langId LIMIT 5', [catgeroyId],function(error, results, fields) {
+            if (error) throw error;
+
+            if (results.length > 0) {
+                console.log(results)
+                resolve(results)
+            }else
+                 reject({response:"",error:"Aucune question n\'a était trouver."})
+        })
+    })
+}
 
 module.exports = router;
